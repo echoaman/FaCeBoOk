507eca6f3faf2c99d032abc146f95359348949cd
echoaman <amanpahurkar13@gmail.com>
2021-04-29 11:01:43 +0530

    auth form entries

--- web/src/components/Login.jsx
+++ web/src/components/Login.jsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
 
 function Login (props) {
+
+	function login() {
+		let formData = new FormData(document.getElementById("login-form"));
+		for(let entry of formData.entries()){
+			console.log(entry);
+		}
+	}
+	
 	return (
 		<div className="auth-container">
 			<div className="auth-header">
 				<span>Login</span>
 			</div>
-			<form action="#" method="get">
+			<form action="#" id="login-form">
 				<input type="email" name="login_email" id="login_email" placeholder="Email" autoComplete="off" required />
 				<input type="password" name="login_password" id="login_password" placeholder="Password" required />
-				<input type="submit" value="Submit"/>
+				<input type="button" value="Submit" onClick={() => login()}/>
 			</form>
 			<div className="auth-footer">
 				<Link to="/signup">Sign Up ?</Link>
--- web/src/components/SignUp.jsx
+++ web/src/components/SignUp.jsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
 
 function SignUp (props) {
+
+	function signup () {
+		let formData = new FormData(document.getElementById("signup-form"));
+		for(let entry of formData.entries()){
+			console.log(entry);
+		}
+	}
+
 	return(
 		<div className="auth-container">
 			<div className="auth-header">
 				<span>Sign Up</span>
 			</div>
-			<form action="#">
+			<form action="#" id="signup-form">
 				<input type="text" name="signup_name" id="signup_name" placeholder="User name" autoComplete="off" required />
 				<input type="email" name="signup_email" id="signup_email" placeholder="Email" autoComplete="off" required />
 				<input type="password" name="signup_password" id="signup_password" placeholder="Password" required />
-				<input type="submit" value="Submit"/>
+				<input type="button" value="Submit" onClick={() => signup()}/>
 			</form>
 			<div className="auth-footer">
 				<Link to="/login">Login ?</Link>
